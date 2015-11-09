17e35950ad3c91eb0ded8e69bd2643ef210acc8a
AbiGeorgieva <abi.georgieva1@gmail.com>
2015-11-09 21:03:56 +0200

    Reverted Test

--- JavaGameProject/src/com/company/Launcher.java
+++ JavaGameProject/src/com/company/Launcher.java
@@ -4,7 +4,7 @@ import Display.Display;
 
 public class Launcher {
     public static void main(String[] args) {
-        //test
+        
         Game game = new Game("Java project", 800, 600);
         game.start();
     }
